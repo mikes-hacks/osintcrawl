6419b1b68d61154b61239be7dea67357594f9002
mikes-hacks <59158016+mikes-hacks@users.noreply.github.com>
2021-06-24 13:15:05 -0400

    Create osintcrawl.sh
    Work in progress..
    Need to add more search locations

---
+++ osintcrawl.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]] ; then
+    echo ''
+    echo 'Usage: osintcrawl.sh megacorpone.com'
+    echo ''
+    exit 1
+fi
+
+if [ "$1" == "-h" ]; then
+    echo ''
+    echo 'Usage: osintcrawl.sh megacorpone.com'
+    echo ''
+    exit 1
+fi
+
+d=`echo $1 | cut -f1 -d'.'`
+
+chk=(
+    https://google.com/search?q=$d
+    https://twitter.com/search?q=$d
+    https://stackoverflow.com/search?q=$d
+)
+
+echo "<HTML><BODY><BR>" > $d.html
+echo "<h1>OSINT Basher</h1>" >> $d.html
+
+for c in "${chk[@]}"; do
+    #cutycapt --url=$c --out=$d_$c.png;
+    echo "$c" >> $d.html
+    echo "<BR><A HREF=\""$c"\"><IMG SRC=\""$d_$c"\" width=600></A><BR><BR>" >> $d.html
+done
+
+echo "</BODY></HTML>" >> $d.html
+
+firefox $d.html
